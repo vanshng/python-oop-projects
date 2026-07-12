bf2391849698b748ff6a598e539ed140fe3d0d04
vansh <vanshn780@gmail.com>
2026-07-12 23:22:01 +0530

    Add Student class with details and introduction methods

--- Student_Management_System/main.py
+++ Student_Management_System/main.py
@@ -1,1 +1,19 @@
 
+class Student:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+    def display_details(self):
+        print(f"Name   : {self.name}")
+        print(f"Age    : {self.age}")
+        print(f"Course : {self.course}")
+
+    def introduce(self):
+        print(f"Hi, I am {self.name} and I am studying {self.course}.")
+
+s1 = Student("Vansh", 20, "BCA")
+
+s1.display_details()
+s1.introduce()
